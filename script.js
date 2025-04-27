304220bb4b358aeea9a87c798564fc6501ba9f27
Ahmed Al-Bahrawy <ahmedalbahrawy-2010@hotmail.com>
2025-04-28 00:38:49 +0300

    Lesson 2

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-console.log("Hello World");
+// old way to make a variable
+var a = 1;
 
-const h1 = document.querySelector("h1");
+// new way to make a variable
+let b = 2;
 
-h1.addEventListener("click", () => {
-    h1.innerHTML = "Hey do not click me";
-});
+// const is a constant variable
+const c = 3;
 
+// by that way I can change one time
+
+// Values ​​change depending on the code order
+let x = "I follow my own dreams";
+console.log(x);
+x = 5;
+console.log(x);
+x = 'Ahmed'
+console.log(x);
+x = 'Al-Bahrawy'
+console.log(x);
+console.log(x);
+x = 5;
+console.log(x);
+x = 'Ahmed'
