4731fffc6cfface754e9e81fac071f183735e18b
Ahmed Al-Bahrawy <ahmedalbahrawy-2010@hotmail.com>
2025-05-04 22:43:28 +0300

    Challenge 3

--- script.js
+++ script.js
@@ -1,49 +1,19 @@
-let title = 'Hello World';
+// Challenge 1:
 
-// substring is used to get a part of a string
-console.log(title.substring(0, 5)); // Hello
-console.log(title.substring(6, 11)); // World
-console.log(title.substring(6)); // World
-console.log(title.substring(6, 6)); // ""
-console.log(title.substring(6, 5)); // ""
+// Create a string variable
+let title = "Hello World";
 
+// Convert the string to uppercase
+title = title.toUpperCase();
+console.log(title);
 
-// length is used to get the length of a string
-console.log(title.length); // 11
+// Replace the first occurrence of 'o' with 'y'
+title = title.replace("o", "y");
+console.log(title);
 
-// upperCase is used to convert a string to uppercase
-console.log(title.toUpperCase()); // HELLO WORLD
-
-// lowerCase is used to convert a string to lowercase
-console.log(title.toLowerCase()); // hello world
-
-// replace is used to replace a part of a string with another string
-console.log(title.replace('Hello', 'Hi')); // Hi World
-
-// split is used to split a string into an array
-console.log(title.split(' ')); // ["Hello", "World"]
-
-
-//charAt is used to get the character at a specific index
-console.log(title.charAt(0)); // H
-
-
-//title[index] is used to get the character at a specific index as like as charAt
-console.log(title[0]); // H
-
-
-//indexOf is used to get the index of a specific character
-console.log(title.indexOf('H')); // 0
-
-let name = 'Ahmed';
-
-console.log(`Hello World, I am ${name}`); // 1
-
-
-
-
-
-
-
-
-
+// Reverse the string by:
+// 1. split('') - converts string to array of characters
+// 2. reverse() - reverses the array
+// 3. join('') - converts array back to string
+title = title.split('').reverse().join('')
+console.log(title);
