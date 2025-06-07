e07afa1569b26c8885c7f1f39736b1d0ba76fcee
Ahmed Al-Bahrawy <ahmedalbahrawy-2010@hotmail.com>
2025-06-07 14:31:12 +0300

    Lesson 9

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
-//Numbers in JavaScript
+// boolean (true or false)
 
-let x = 20;
-let y = 10;
+let x = true;
+let y = false;
+let z = null;
+let a = undefined;
 
-let z = x * y + 30; // 230 follow the math rules
+console.log(x); // true
+console.log(y); // false
+console.log(z); // null
+console.log(a); // undefined
 
-z = 30 + x * y; // 230 follow the math rules (BODMAS)
+console.log("=".repeat(50));
 
-z = (30 + x) * y; // 600 follow the math rules (BODMAS)
+x = 10;
+y = 20;
 
-z = 30 + x * y; // 630 follow the math rules (BODMAS)
+console.log(x > y); // false
+console.log(x < y); // true
+console.log(x >= y); // false
+console.log(x <= y); // true
+console.log(x == y); // false
+console.log(x != y); // true
+console.log(x === y); // false
+console.log(x !== y); // true
 
-// isNaN()
-
-console.log(isNaN(x)); // true
-console.log(isNaN(y)); // true
-console.log(isNaN(z)); // true
-
-// Math Object
-
-console.log(Math.PI); // 3.141592653589793
-
-console.log(Math.round(4.6)); // 5
-console.log(Math.round(4.4)); // 4
-
-console.log(Math.min(x, y, z)); // 10
-console.log(Math.max(x, y, z)); // 230
-
-x = "10";
-y = "20";
-
-// z = x * y // NaN (Not a Number)
-
-z = x + y; // 1020 (concatenation)
-
-// isNaN()
-
-console.log(isNaN(x)); // false
-console.log(isNaN(y)); // false
-console.log(isNaN(z)); // false
+console.log("=".repeat(50));
