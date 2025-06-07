d42cbbe818b5131726d11d41917166dd1892acc0
Ahmed Al-Bahrawy <ahmedalbahrawy-2010@hotmail.com>
2025-06-07 14:02:55 +0300

    Lesson 8

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-// Challenge 2:
+//Numbers in JavaScript
 
-const text = document.getElementById('text');
-const r1 = document.getElementById('r-1');
-const r2 = document.getElementById('r-2');
-const r3 = document.getElementById('r-3');
+let x = 20;
+let y = 10;
 
-r1.innerHTML = text.innerHTML.toUpperCase();
-r2.innerHTML = text.innerHTML.replace("o", "y");
-r3.innerHTML = text.innerHTML.split('').reverse().join('');
+let z = x * y + 30; // 230 follow the math rules
 
+z = 30 + x * y; // 230 follow the math rules (BODMAS)
 
+z = (30 + x) * y; // 600 follow the math rules (BODMAS)
 
+z = 30 + x * y; // 630 follow the math rules (BODMAS)
 
+// isNaN()
 
+console.log(isNaN(x)); // true
+console.log(isNaN(y)); // true
+console.log(isNaN(z)); // true
 
+// Math Object
 
+console.log(Math.PI); // 3.141592653589793
+
+console.log(Math.round(4.6)); // 5
+console.log(Math.round(4.4)); // 4
+
+console.log(Math.min(x, y, z)); // 10
+console.log(Math.max(x, y, z)); // 230
+
+x = "10";
+y = "20";
+
+// z = x * y // NaN (Not a Number)
+
+z = x + y; // 1020 (concatenation)
+
+// isNaN()
+
+console.log(isNaN(x)); // false
+console.log(isNaN(y)); // false
+console.log(isNaN(z)); // false
