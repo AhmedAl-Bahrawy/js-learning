973d8c9514235d496332a61587c0afe449ad46cb
Ahmed Al-Bahrawy <ahmedalbahrawy-2010@hotmail.com>
2025-06-09 19:21:39 +0300

    Challenge 6

--- script.js
+++ script.js
@@ -1,58 +1,24 @@
-// // Arrays
+// Challenge 6
+let names = [];
 
-// let students = ["Ahmed", "Mohammed", "Omar", "Anas", "Osama"];
+const add_btn = document
+  .getElementById("add")
+  .addEventListener("click", function () {
+    const name = document.getElementById("name");
+    const names_list_HTML = document.getElementById("names-list");
+    if (name.value != "") {
+      names.push(name.value);
+      names_list_HTML.innerHTML = names;
+      name.value = "";
+    } else {
+      alert("please enter a name ");
+    }
+  });
 
-// students.push("Hamza");
-// // students[2] = "Ammar";
-// // let student = students[2];
-// // students[2] = students[4];
-// // students[4] = student;
-
-// document.getElementById("students").innerHTML = students;
-
-// // console.log(students);
-// // console.log(students.length);
-// // console.log(students[2]);
-// // students.pop();
-// // console.log(students);
-
-// console.log(students);
-// // replace index 2 with Hosam
-// students.splice(2, 1, "Hosam");
-// console.log(students);
-// // remove index 2
-// students.splice(2, 1);
-// console.log(students);
-// // remove index 1, 2
-// students.splice(1, 2);
-// console.log(students);
-// // add Odii in index 2
-// students.splice(2, 0, "Odii");
-
-// let str = "Sting";
-// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90];
-// // is Array or Not
-// console.log(Array.isArray(str)); // flase
-// console.log(Array.isArray(arr)); // true
-
-// // is Array includes a value or Not
-// console.log(arr.includes(10)); // true
-// console.log(arr.includes(22)); // false
-
-// // to get the index of value
-// console.log(arr.indexOf(50)); // 4
-// console.log(arr.indexOf(100)); // -1
-
-// // to remove first value of array
-// console.log(arr);
-// console.log(arr.shift());
-// console.log(arr);
-
-// // to add value in first of array
-// console.log(arr);
-// console.log(arr.unshift(10));
-// console.log(arr);
-
-// // to reverse the index of array
-// console.log(arr);
-// console.log(arr.reverse());
+const delete_btn = document
+  .getElementById("delete")
+  .addEventListener("click", function () {
+    const names_list_HTML = document.getElementById("names-list");
+    names.pop();
+    names_list_HTML.innerHTML = names;
+  });
