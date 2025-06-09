27eabad2629eac063ed9eb97915346f84076183f
Ahmed Al-Bahrawy <ahmedalbahrawy-2010@hotmail.com>
2025-06-09 17:57:54 +0300

    Lesson 18

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
-// Arrays
+// // Arrays
 
-let students = ["Ahmed", "Mohammed", "Omar", "Anas", "Osama"];
+// let students = ["Ahmed", "Mohammed", "Omar", "Anas", "Osama"];
 
-students.push("Hamza");
-// students[2] = "Ammar";
-// let student = students[2];
-// students[2] = students[4];
-// students[4] = student;
+// students.push("Hamza");
+// // students[2] = "Ammar";
+// // let student = students[2];
+// // students[2] = students[4];
+// // students[4] = student;
 
-document.getElementById("students").innerHTML = students;
+// document.getElementById("students").innerHTML = students;
+
+// // console.log(students);
+// // console.log(students.length);
+// // console.log(students[2]);
+// // students.pop();
+// // console.log(students);
 
 // console.log(students);
-// console.log(students.length);
-// console.log(students[2]);
-// students.pop();
+// // replace index 2 with Hosam
+// students.splice(2, 1, "Hosam");
 // console.log(students);
+// // remove index 2
+// students.splice(2, 1);
+// console.log(students);
+// // remove index 1, 2
+// students.splice(1, 2);
+// console.log(students);
+// // add Odii in index 2
+// students.splice(2, 0, "Odii");
 
-console.log(students);
-// replace index 2 with Hosam
-students.splice(2, 1, "Hosam");
-console.log(students);
-// remove index 2
-students.splice(2, 1);
-console.log(students);
-// remove index 1, 2
-students.splice(1, 2);
-console.log(students);
-// add Odii in index 2
-students.splice(2, 0, "Odii");
+// let str = "Sting";
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+// // is Array or Not
+// console.log(Array.isArray(str)); // flase
+// console.log(Array.isArray(arr)); // true
+
+// // is Array includes a value or Not
+// console.log(arr.includes(10)); // true
+// console.log(arr.includes(22)); // false
+
+// // to get the index of value
+// console.log(arr.indexOf(50)); // 4
+// console.log(arr.indexOf(100)); // -1
+
+// // to remove first value of array
+// console.log(arr);
+// console.log(arr.shift());
+// console.log(arr);
+
+// // to add value in first of array
+// console.log(arr);
+// console.log(arr.unshift(10));
+// console.log(arr);
+
+// // to reverse the index of array
+// console.log(arr);
+// console.log(arr.reverse());
