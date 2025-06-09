8842d25e8232c9e3ea2bbd838c87c37e6a747a4c
Ahmed Al-Bahrawy <ahmedalbahrawy-2010@hotmail.com>
2025-06-09 11:56:47 +0300

    Lesson 15

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // Switch & Case
 
-let health = "strong";
+let age = 60;
 
-switch (health) {
-  case "strong":
-    console.log("Still strong forever");
+// switch (health) {
+//   case "strong":
+//     console.log("Still strong forever");
+//     break;
+//   case "weak":
+//     console.log("Watch out your health");
+//     break;
+//   default:
+//     console.log("good");
+// }
+
+switch (true) {
+  case age >= 60:
+    console.log("You are old");
     break;
-  case "weak":
-    console.log("Watch out your health");
+  case age >= 40:
+    console.log("You are near to be old");
     break;
   default:
-    console.log("good");
+    console.log("You are Young");
 }
